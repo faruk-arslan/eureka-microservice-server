0a435915c0f21bfcc961a641fa3d3f594206479c
Faruk Arslan <farukarslan011@gmail.com>
2021-07-22 13:27:19 +0300

    Add properties for eureka server (port etc.). Add @EnableEurekaServer annotation to the Main Class.

--- src/main/java/com/fa/eurekamicroservice/EurekaMicroserviceApplication.java
+++ src/main/java/com/fa/eurekamicroservice/EurekaMicroserviceApplication.java
@@ -2,8 +2,10 @@ package com.fa.eurekamicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaMicroserviceApplication {
 
 	public static void main(String[] args) {
